160ed62e05a2fce967c1501f14842d59024ece61
Alexey Penskikh <point255@gmail.com>
2019-01-03 16:18:17 +0700

    Update setup information.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     setup(
         name='pg_rabbitmq',
         description='PostgreSQL RabbitMQ Foreign Data Wrapper (write only)',
-        version='1.0.2',
-        author='20 Minutes',
+        version='1.1.0',
+        author='point255',
         license='MIT',
         packages=['pg_rabbitmq'],
-        url='https://github.com/20minutes/pg-rabbitmq-fdw',
+        url='https://github.com/point255/pg-rabbitmq-fdw',
         test_suite="tests"
     )
